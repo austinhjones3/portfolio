59895f6281093acbcfb951d2cb47909f15fce4a8
Austin Jones <75518099+austinhjones3@users.noreply.github.com>
2020-12-25 12:27:20 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var host = "YOURDOMAIN.github.io"
+var host = "austinjones.io"
 if (window.location.host == host && window.location.protocol != "https:") {
     window.location.protocol = "https:"
 }
